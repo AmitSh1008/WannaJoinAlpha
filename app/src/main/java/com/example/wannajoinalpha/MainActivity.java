3f16e2be60f2313110304a621c5d76d8486b47bf
AmitSh1008 <aamitshilman1008@gmail.com>
2023-11-29 23:27:35 +0200

    Broadcast Reciever and Wifi Check

--- app/src/main/java/com/example/wannajoinalpha/MainActivity.java
+++ app/src/main/java/com/example/wannajoinalpha/MainActivity.java
@@ -3,7 +3,11 @@ package com.example.wannajoinalpha;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.wifi.WifiManager;
 import android.os.Bundle;
 import android.util.Patterns;
 import android.view.View;
@@ -19,6 +23,8 @@ import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
+    private WifiManager wifiManager;
+
     private FirebaseAuth auth;
     private EditText etEmail, etPassword;
     private Button btnLogin;
@@ -29,6 +35,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        wifiManager = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+
         auth = FirebaseAuth.getInstance();
         etEmail = findViewById(R.id.editTextEmail);
         etPassword = findViewById(R.id.editTextPassword);
@@ -38,36 +46,38 @@ public class MainActivity extends AppCompatActivity {
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String email = etEmail.getText().toString();
-                String password = etPassword.getText().toString();
 
-                if (!email.isEmpty() && Patterns.EMAIL_ADDRESS.matcher(email).matches())
+                if (wifiManager.isWifiEnabled())
                 {
-                    if (!password.isEmpty())
-                    {
-                        auth.signInWithEmailAndPassword(email, password)
-                                .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
-                                    @Override
-                                    public void onSuccess(AuthResult authResult) {
-                                        Toast.makeText(MainActivity.this, "Login Successful!", Toast.LENGTH_SHORT).show();
-                                    }
-                                })
-                                .addOnFailureListener(new OnFailureListener() {
-                                    @Override
-                                    public void onFailure(@NonNull Exception e) {
-                                        Toast.makeText(MainActivity.this, "Login Failed!", Toast.LENGTH_SHORT).show();
-                                    }
-                                });
+                    String email = etEmail.getText().toString();
+                    String password = etPassword.getText().toString();
+
+                    if (!email.isEmpty() && Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+                        if (!password.isEmpty()) {
+                            auth.signInWithEmailAndPassword(email, password)
+                                    .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                                        @Override
+                                        public void onSuccess(AuthResult authResult) {
+                                            Toast.makeText(MainActivity.this, "Login Successful!", Toast.LENGTH_SHORT).show();
+                                        }
+                                    })
+                                    .addOnFailureListener(new OnFailureListener() {
+                                        @Override
+                                        public void onFailure(@NonNull Exception e) {
+                                            Toast.makeText(MainActivity.this, "Login Failed!", Toast.LENGTH_SHORT).show();
+                                        }
+                                    });
+                        } else {
+                            etPassword.setError("password cannot be empty!");
+                        }
+                    } else if (email.isEmpty()) {
+                        etEmail.setError("email cannot be empty!");
+                    } else {
+                        etEmail.setError("please enter valid email!");
                     }
-                    else {
-                        etPassword.setError("password cannot be empty!");
-                    }
-                }
-                else if (email.isEmpty()) {
-                    etEmail.setError("email cannot be empty!");
                 }
                 else {
-                    etEmail.setError("please enter valid email!");
+                    Toast.makeText(MainActivity.this, "Your Wifi is OFF!", Toast.LENGTH_SHORT).show();
                 }
             }
         });
@@ -78,5 +88,36 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(new Intent(MainActivity.this, SignUpActivity.class));
             }
         });
+
     }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        IntentFilter intentFilter = new IntentFilter(WifiManager.WIFI_STATE_CHANGED_ACTION);
+        registerReceiver(wifiStateReceiver, intentFilter);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        unregisterReceiver(wifiStateReceiver);
+    }
+
+    private BroadcastReceiver wifiStateReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            int wifiStateExtra = intent.getIntExtra(WifiManager.EXTRA_WIFI_STATE,
+                    WifiManager.WIFI_STATE_UNKNOWN);
+
+            switch (wifiStateExtra) {
+                case WifiManager.WIFI_STATE_ENABLED:
+                    Toast.makeText(MainActivity.this, "Wifi is ON!", Toast.LENGTH_LONG).show();
+                    break;
+                case WifiManager.WIFI_STATE_DISABLED:
+                    Toast.makeText(MainActivity.this, "Wifi is OFF!", Toast.LENGTH_LONG).show();
+                    break;
+            }
+        }
+    };
+
 }
--- app/src/main/java/com/example/wannajoinalpha/SignUpActivity.java
+++ app/src/main/java/com/example/wannajoinalpha/SignUpActivity.java
@@ -3,7 +3,11 @@ package com.example.wannajoinalpha;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.wifi.WifiManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -18,6 +22,8 @@ import com.google.firebase.auth.FirebaseAuth;
 
 public class SignUpActivity extends AppCompatActivity {
 
+    private WifiManager wifiManager;
+
     private FirebaseAuth auth;
     private EditText etUsername, etEmail, etPassword, etConPassword, etPhoneNum;
     private Button btnSignup;
@@ -27,6 +33,8 @@ public class SignUpActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        wifiManager = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
 
         auth = FirebaseAuth.getInstance();
         etUsername = findViewById(R.id.editTextUserName);
@@ -40,59 +48,66 @@ public class SignUpActivity extends AppCompatActivity {
         btnSignup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String username = etUsername.getText().toString().trim();
-                String email = etEmail.getText().toString().trim();
-                String password = etPassword.getText().toString().trim();
-                String conPassword = etConPassword.getText().toString().trim();
-                String phoneNum = etPhoneNum.getText().toString().trim();
 
-                boolean allValid = true;
-                if (username.isEmpty())
+                if (wifiManager.isWifiEnabled())
                 {
-                    etUsername.setError("username cannot be empty!");
-                    allValid = false;
-                }
-                if (email.isEmpty())
-                {
-                    etEmail.setError("email cannot be empty!");
-                    allValid = false;
-                }
-                if (password.isEmpty())
-                {
-                    etPassword.setError("password cannot be empty!");
-                    allValid = false;
-                }
-                if (conPassword.isEmpty())
-                {
-                    etConPassword.setError("confirm password cannot be empty!");
-                    allValid = false;
-                }
-                if (phoneNum.isEmpty())
-                {
-                    etPhoneNum.setError("phone number cannot be empty!");
-                    allValid = false;
-                }
-                if (allValid)
-                {
-                    if (password.equals(conPassword))
+                    String username = etUsername.getText().toString().trim();
+                    String email = etEmail.getText().toString().trim();
+                    String password = etPassword.getText().toString().trim();
+                    String conPassword = etConPassword.getText().toString().trim();
+                    String phoneNum = etPhoneNum.getText().toString().trim();
+
+                    boolean allValid = true;
+                    if (username.isEmpty())
                     {
-                        auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                            @Override
-                            public void onComplete(@NonNull Task<AuthResult> task) {
-                                if (task.isSuccessful())
-                                {
-                                    Toast.makeText(SignUpActivity.this, "Signup Successful!", Toast.LENGTH_SHORT).show();
-                                    startActivity(new Intent(SignUpActivity.this, MainActivity.class));
-                                }
-                                else {
-                                    Toast.makeText(SignUpActivity.this, "Signup Failed " + task.getException().getMessage() + '!', Toast.LENGTH_SHORT).show();
-                                }
-                            }
-                        });
+                        etUsername.setError("username cannot be empty!");
+                        allValid = false;
                     }
-                    else {
-                        etConPassword.setError("password does not match!");
+                    if (email.isEmpty())
+                    {
+                        etEmail.setError("email cannot be empty!");
+                        allValid = false;
                     }
+                    if (password.isEmpty())
+                    {
+                        etPassword.setError("password cannot be empty!");
+                        allValid = false;
+                    }
+                    if (conPassword.isEmpty())
+                    {
+                        etConPassword.setError("confirm password cannot be empty!");
+                        allValid = false;
+                    }
+                    if (phoneNum.isEmpty())
+                    {
+                        etPhoneNum.setError("phone number cannot be empty!");
+                        allValid = false;
+                    }
+                    if (allValid)
+                    {
+                        if (password.equals(conPassword))
+                        {
+                            auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if (task.isSuccessful())
+                                    {
+                                        Toast.makeText(SignUpActivity.this, "Signup Successful!", Toast.LENGTH_SHORT).show();
+                                        startActivity(new Intent(SignUpActivity.this, MainActivity.class));
+                                    }
+                                    else {
+                                        Toast.makeText(SignUpActivity.this, "Signup Failed " + task.getException().getMessage() + '!', Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                            });
+                        }
+                        else {
+                            etConPassword.setError("password does not match!");
+                        }
+                    }
+                }
+                else {
+                    Toast.makeText(SignUpActivity.this, "Your Wifi is OFF!", Toast.LENGTH_SHORT).show();
                 }
             }
         });
@@ -104,4 +119,34 @@ public class SignUpActivity extends AppCompatActivity {
             }
         });
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        IntentFilter intentFilter = new IntentFilter(WifiManager.WIFI_STATE_CHANGED_ACTION);
+        registerReceiver(wifiStateReceiver, intentFilter);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        unregisterReceiver(wifiStateReceiver);
+    }
+
+    private BroadcastReceiver wifiStateReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            int wifiStateExtra = intent.getIntExtra(WifiManager.EXTRA_WIFI_STATE,
+                    WifiManager.WIFI_STATE_UNKNOWN);
+
+            switch (wifiStateExtra) {
+                case WifiManager.WIFI_STATE_ENABLED:
+                    Toast.makeText(SignUpActivity.this, "Wifi is ON!", Toast.LENGTH_LONG).show();
+                    break;
+                case WifiManager.WIFI_STATE_DISABLED:
+                    Toast.makeText(SignUpActivity.this, "Wifi is OFF!", Toast.LENGTH_LONG).show();
+                    break;
+            }
+        }
+    };
 }
